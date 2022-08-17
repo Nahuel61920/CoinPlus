7174ebeced65f22f885ec0ff9a12fd50543af86f
unknown <pjendara@gmail.com>
2022-08-17 18:58:12 -0500

    Inicio Header-main

--- Client/src/components/Main/Main.jsx
+++ Client/src/components/Main/Main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from "../Sections/Header/Header"
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className="container-fluid">
+    <Header/>
+    </div>
+    
   )
 }
 
--- Client/src/page/LandingPage/LandingPage.jsx
+++ Client/src/page/LandingPage/LandingPage.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import Nav from '../../components/Nav/Nav'
+import React, { Fragment } from 'react'
+import Nav from "../../components/Nav/Nav"
+import Main from "../../components/Main/Main"
+import Footer from "../../components/Footer/Footer"
 
 function LandingPage() {
   return (
+    <>
     <Nav/>
+    <Main/>
+    <Footer/>
+    </>
+
   )
 }
 
