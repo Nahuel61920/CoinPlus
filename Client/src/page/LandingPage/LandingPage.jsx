3814071f3ea228a961c2ecf203599375c9cee1c3
teyuu <tellini.matias@gmail.com>
2022-08-17 20:35:33 -0300

    footer

--- Client/src/page/LandingPage/LandingPage.jsx
+++ Client/src/page/LandingPage/LandingPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import Nav from '../../components/Nav/Nav'
+import Nav from '../../components/Nav/Nav';
+import Footer from '../../components/Footer/Footer';
 
 function LandingPage() {
   return (
-    <Nav/>
+    <>
+    <Nav/>,
+    <Footer/>
+    </>
   )
 }
 
