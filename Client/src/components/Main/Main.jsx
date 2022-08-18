3703684754da6e6e799a62188a1c78dcf5aacd67
laranlopez <laranahirlopez@gmail.com>
2022-08-18 20:24:55 -0300

    empezamos tobepart

--- Client/src/components/Main/Main.jsx
+++ Client/src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from "../Sections/Header/Header"
 import Services from "../Sections/Services/Services"
+import Tobepart from "../Sections/ToBePart/tobepart"
 
 function Main() {
   return (
     <div className="container-xl">
       <Header/>
       <Services/>
+      <Tobepart/>
     </div>
   )
 }
