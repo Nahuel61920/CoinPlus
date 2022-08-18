f2ac6acce5c70577bdb3db365412507eab3d1b7b
TomassCast99 <86583636+TomassCast99@users.noreply.github.com>
2022-08-17 21:04:37 -0300

    DB connected and re-estructuring

--- Api/src/schemas/User.ts
+++ Api/src/schemas/User.ts
@@ -1,33 +1,53 @@
-const {mongooseUser} = require('../index.ts')
+import { prop, getModelForClass, Ref, modelOptions } from '@typegoose/typegoose';
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 
-const userSchema = new mongooseUser.Schema(
-    {
-        name: {
-            type: String,
-            require: true
-        },
-        email: {
-            type: String,
-            unique: true
-        },
-        numberPhone: {
-            type: Number,
-            default: '123-456'
-        },
-        tag: {
+@modelOptions({ options: { allowMixed: 0 } })
+class User extends TimeStamps{
+    @prop({ required: true, trim: true})
+    name: string;
 
-            type: Array,
-            default: []
-        }
-    },
-    {
-        timestamps: true,
-        versionKey: false
-    }
-)
+    @prop({ required: false, trim: true, default: "" })
+    middleName?: string;
 
-const User = new mongooseUser.model('user', userSchema)
+    @prop({ required: true, trim: true})
+    lastName: string;
 
-//User.create({name:"Tomi", email:"algo@gmail.com", numberPhone:135135, tag:["queseyo"]})
+    @prop({ required: true, trim: true, lowercase:true})
+    email:string;
 
-module.exports = { User }
+    @prop({ required: false,  default: 123-456 })
+    numberPhone: number;
+
+    @prop({ type: () => Array, default: [] })
+    favourites: Array<string>;
+    //agregar la Ref del archivo Crypto.ts
+
+    @prop({ required: false, default: "https://media.discordapp.net/attachments/1005186345397927979/1009546767739658280/unknown.png" })
+    picture:string;
+
+    @prop({ required: true, trim: true, lowercase:true, minlength:true })
+    password:string;
+
+    @prop({ type: () => Boolean, default: false})
+    isBanned: boolean;
+
+    @prop({ required: false, trim: true, default: "" })
+    country:string;
+
+    @prop({ required: false,  default: 1234 })
+    postalCod: number;
+
+    @prop({required: false, trim: true})
+    adress: string;
+
+    @prop()
+    documentType: string;
+
+    @prop()
+    documentNum: number;
+}
+
+const UserModel = getModelForClass(User)
+
+export default UserModel
+
