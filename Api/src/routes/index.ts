638bc6925fadccafd366789d03fc5d8bd187a16c
anthony29p <anthony.perez@uni.pe>
2022-08-19 18:10:36 -0500

    mas rutas

--- Api/src/routes/index.ts
+++ Api/src/routes/index.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router();
 const  getAllCryptos = require ("./getAllCryptos")
 const getAllDetail= require("./getAllDetail")
+const  userRoute = require ("./user")
 
+router.use("/profile", userRoute)
 router.use("/market", getAllCryptos)
 router.use("/detail", getAllDetail)
 router.use("/", (req, res)=> res.send("COIN+, EL BACK YA FUNCIONA :)"))
