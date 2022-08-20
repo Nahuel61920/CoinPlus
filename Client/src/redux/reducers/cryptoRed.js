0e1a63eb5563d8e29c626d5c3022e89f0e43c5b9
TomassCast99 <86583636+TomassCast99@users.noreply.github.com>
2022-08-20 19:43:16 -0300

    Detail no complete, connection reducer

--- Client/src/redux/reducers/cryptoRed.js
+++ Client/src/redux/reducers/cryptoRed.js
@@ -5,15 +5,20 @@ export const cryptoSlice = createSlice({
   name: "crypto",
   initialState: {
     cryptos: [],
+    details: {},
   },
   reducers: {
     setCryptoList: (state, { type, payload }) => {
       state.cryptos = payload;
     },
+    cryptoDetail: (state, { type, payload }) => {
+      console.log(payload);
+      state.details = payload;
+    },
   },
 });
 
-export const { setCryptoList } = cryptoSlice.actions;
+export const { setCryptoList, cryptoDetail } = cryptoSlice.actions;
 
 export default cryptoSlice.reducer;
 
@@ -22,6 +27,16 @@ export const fetchCrypto = () => (dispatch) => {
     .get("http://localhost:3001/market")
     .then((res) => {
       dispatch(setCryptoList(res.data));
+    })
+    .catch((err) => console.log(err));
+};
+
+export const detailCrypto = (id) => (dispatch) => {
+  console.log(id);
+  axios
+    .get(`http://localhost:3001/crypto/${id}`)
+    .then((res) => {
+      dispatch(cryptoDetail(res.data));
     })
     .catch((err) => console.log(err));
 };
