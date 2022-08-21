85aec5395dc1dbc660ddb2c25bfa84d34d7a790d
laranlopez <laranahirlopez@gmail.com>
2022-08-21 20:10:04 -0300

    searchbar

--- Client/src/components/Nav/NavAl.jsx
+++ Client/src/components/Nav/NavAl.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import Logo from '../../assets/img/coin+logo.png'
 import { Link } from 'react-scroll';
 import { NavLink } from 'react-router-dom';
-import './nav.css' 
+import './nav.css' ;
+
 
 import { useAuth0 } from '@auth0/auth0-react';
 
 import LoginButton from '../Login/LoginButton';
 import ImgProfile from '../Profile/ImgProfile';
+import { Cryptoname, nameCrypto } from '../../redux/reducers/cryptoRed';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function NavAl() {
 
   const {isAuthenticated} = useAuth0()
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
 
   return (
     <nav class="navbar navbar-expand-sm navbar-dark bg-warning px-md-5 px-1 sticky-top">
@@ -19,6 +25,14 @@ function NavAl() {
           <NavLink to ="/">
           <img src={Logo} alt="C+" width="150"/>
           </NavLink>
+          <div>
+          <div class="container-fluid">
+<form class="d-flex " role="search">
+  <input class="form-control me-2 hight" type="search" placeholder="Search..." aria-label="Search"/>
+  <button class="buttonLogin" type="submit"  onClick={(e) => handleSubmit(e)}>Search</button>
+</form>
+</div>
+         </div>
             <button class="navbar-toggler" 
                 type="button" 
                 data-bs-toggle="collapse" 
@@ -32,12 +46,17 @@ function NavAl() {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="nav navbar-nav ms-auto mx-3"/>
                 <ImgProfile/>
-
-
-            </div>
+                </div>
         </div>
     </nav>
   )
+  function handleSubmit(e){
+    e.preventDefault();
+      dispatch(Cryptoname(name));
+  
+  };
+  
 }
+
 
 export default NavAl
