09d3f87968e0026cf9e1714beea9519c7c989a6a
TomassCast99 <86583636+TomassCast99@users.noreply.github.com>
2022-08-26 16:30:48 -0300

    wallet connection complete

--- Client/src/utils/constants.js
+++ Client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x563b2E1aAa4deD43E77A4Ec82f7f75afb1219970";
 export const contractABI = abi.abi;
