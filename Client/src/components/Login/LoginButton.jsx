1b2a8d5da3d502fdc58c1afc61b676aa068297fd
Nahuel61920 <estebancarrizo619@gmail.com>
2022-08-27 18:44:09 -0300

    media queri landing

--- Client/src/components/Login/LoginButton.jsx
+++ Client/src/components/Login/LoginButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
 import {FaUserAltSlash} from 'react-icons/fa';
+import "./loginbtn.css"
 
 
 export default function LoginButton() {
@@ -9,13 +10,13 @@ export default function LoginButton() {
 
   return (
     <>
-    <button onClick={() => loginWithRedirect()} className="buttonLogin">
+    <button onClick={() => loginWithRedirect()} className="buttonLogin buttonLogSin">
       Login/SignUp
       <div class="arrow-wrapper">
           <div class="arrow"></div>
       </div>
     </button>
-    <div onClick={() => loginWithRedirect()}>
+    <div onClick={() => loginWithRedirect()} className="buttonLoginMq">
       <FaUserAltSlash className="iconLogin"/>
     </div>
     </>
--- Client/src/components/Sections/Header/Header.jsx
+++ Client/src/components/Sections/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
   const {isAuthenticated} = useAuth0()
   return (
     <div id="Home" className="row aling-content-center justify-content-center d-flex flex-md-row flex-column min-vh-100">
-      <div className="col-12 col-md-6 aling-content-center mt-5" data-aos="fade-right" data-aos-delay="300">
+      <div className="col-12 col-md-6 aling-content-center mt-5 contenedor-header" data-aos="fade-right" data-aos-delay="300">
         <h4 className="fs-4 mt-5">Tu ruta hacia el futuro.</h4>
         <h1 className="title__text fw-bold my-4">Acércate al mundo cripto.</h1>
         <p className="fs-4">La forma más simple y segura de comerciar criptomonedas.</p>
