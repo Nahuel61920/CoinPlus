352e557c2e68b7ebfaa69f4242893d1e3dcb1b6a
TomassCast99 <86583636+TomassCast99@users.noreply.github.com>
2022-08-28 12:57:03 -0300

    fix login

--- Client/src/components/Login/LoginDetailBtn.jsx
+++ Client/src/components/Login/LoginDetailBtn.jsx
@@ -1,48 +1,59 @@
-import React, {useState} from 'react'
-import './loginbtn.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./loginbtn.css";
+import { Link } from "react-router-dom";
 import { createUser } from "../../redux/reducers/cryptoRed";
 import { useSelector, useDispatch } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function LoginDetailBtn() {
+  const { user } = useAuth0();
 
-  const { user} = useAuth0();
+  const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.crypto);
 
-  const dispatch= useDispatch()
-  const {users}= useSelector((state)=> state.crypto)
-
-  const [input, setInput]= useState({
+  const [input, setInput] = useState({
     name: "",
     email: "",
     nickname: "",
     picture: "",
     source: "",
-  })
-  
-  function HandleCreate(input){
-    
-    setInput({
-    name: user.given_name,
-    email: user.email,
-    nickname: user.nickname,
-    picture: user.picture,
-    source: user.sub.toString(),
-    })
-  
-    console.log("input--->",input)
-    dispatch(createUser(input))
-    
+  });
+
+  function HandleCreate(e) {
+    const a = users.filter((b) => b.users.email === e.users.email);
+    if (a.length > 0) {
+      return alert("There is a pokémon with that name, try another");
+    }
+
+    let crear = {
+      name: user.given_name,
+      email: user.email,
+      nickname: user.nickname,
+      picture: user.picture,
+      source: user.sub.toString(),
+    };
+
+    dispatch(createUser(crear));
+
+    console.log("input--->", crear);
+
+    // setInput({
+    //   name: user.given_name,
+    //   email: user.email,
+    //   nickname: user.nickname,
+    //   picture: user.picture,
+    //   source: user.sub.toString(),
+    // });
   }
-  
+
   return (
-    <Link to="/user" style={{ textDecoration: 'none' }}>
-        <button className="buttonLogin" onClick={HandleCreate(input)}>
+    <Link to="/user" style={{ textDecoration: "none" }}>
+      <button className="buttonLogin" onClick={(e) => HandleCreate(e)}>
         Perfil
         <div class="arrow-wrapper">
-            <div class="arrow"></div>
+          <div class="arrow"></div>
         </div>
-        </button>
+      </button>
     </Link>
-  )
+  );
 }
