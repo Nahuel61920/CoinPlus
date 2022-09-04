c98ab7d61c35252891a059ba7a722ec3ca400d2b
teyuu <tellini.matias@gmail.com>
2022-09-03 22:18:40 -0300

    realice operation detail y agregue sweet alert

--- Client/src/components/Profile/billetera/Billetera.jsx
+++ Client/src/components/Profile/billetera/Billetera.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import NavBar from '../../Nav/NavProfile'
 import Metamask from '../../Metamask/Metamask'
 import Convertidor from '../../Convertidor/Convertidor'
+import OperationDetail from '../../Convertidor/OperationDetail'
 
 function Billetera() {
   return (
     <div>
         <NavBar/>
         <Convertidor></Convertidor>
-        <Metamask/>
+        <OperationDetail/>
+        <Metamask/> 
     </div>
   )
 }
