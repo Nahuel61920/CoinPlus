a62ebe1b8c696a700f35abe237664f460e5ec2f2
TomassCast99 <86583636+TomassCast99@users.noreply.github.com>
2022-09-07 15:41:37 -0300

    FIX Pag, MArket and Review

--- Client/src/components/Sections/Review/Review.jsx
+++ Client/src/components/Sections/Review/Review.jsx
@@ -12,14 +12,13 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 export default function Review() {
-    const dispatch = useDispatch();
-    const { commets } = useSelector((state) => state.crypto);
+  const dispatch = useDispatch();
+  const { commets } = useSelector((state) => state.crypto);
 
-    useEffect(() => {
-        dispatch(fetchCommet());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchCommet());
+  }, [dispatch]);
 
-    console.log(commets);
   return (
     <div
       id="Reviews"
@@ -32,61 +31,62 @@ export default function Review() {
         data-aos-delay="300"
       >
         <Swiper
-            spaceBetween={30}
-            slidesPerView={1}
-            navigation={{
-                nextEl: ".next",
-                prevEl: ".prev",
-            }}
-            loop={true}
-            modules={[Navigation]}
-            className="mySwiper"
-            >
-            <div>
+          spaceBetween={30}
+          slidesPerView={1}
+          navigation={{
+            nextEl: ".next",
+            prevEl: ".prev",
+          }}
+          loop={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <div>
+            {commets.map((commet) => (
+              <SwiperSlide key={commet.id}>
+                <div className="testimonial__quote">
+                  <ImQuotesLeft className="bx bxs-quote-alt-left"></ImQuotesLeft>
+                </div>
+                <p className="testimonial__description">{commet.content}</p>
+                <h3 className="testimonial__date">
+                  {commet.createdAt.slice(0, 10)}
+                </h3>
+                <div className="testimonial__perfil">
+                  <img
+                    src={commet.picture}
+                    alt=""
+                    className="testimonial__perfil-img"
+                  />
 
-
-                {commets.map((commet) => (
-                    <SwiperSlide key={commet.id}>
-                        <div className="testimonial__quote">
-                            <ImQuotesLeft className='bx bxs-quote-alt-left' ></ImQuotesLeft>
-                        </div>
-                        <p className="testimonial__description">
-                            {commet.content}
-                        </p>
-                        <h3 className="testimonial__date">{commet.createdAt.slice(0, 10)}</h3>
-                        <div className="testimonial__perfil">
-                            <img src={commet.picture} alt="" className="testimonial__perfil-img"/>
-
-                            <div className="testimonial__perfil-data">
-                                <span className="testimonial__perfil-name">{commet.name}</span>
-                                    {commet.rating === 1 ? (
-                                        <span className="testimonial__perfil-stars">★</span>
-                                    ) : commet.rating === 2 ? (
-                                        <span className="testimonial__perfil-stars">★★</span>
-                                    ) : commet.rating === 3 ? (
-                                        <span className="testimonial__perfil-stars">★★★</span>
-                                    ) : commet.rating === 4 ? (
-                                        <span className="testimonial__perfil-stars">★★★★</span>
-                                    ) : commet.rating === 5 ? (
-                                        <span className="testimonial__perfil-stars">★★★★★</span>
-                                    ) 
-                                    : null}
-
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                ))}
+                  <div className="testimonial__perfil-data">
+                    <span className="testimonial__perfil-name">
+                      {commet.name}
+                    </span>
+                    {commet.rating === 1 ? (
+                      <span className="testimonial__perfil-stars">★</span>
+                    ) : commet.rating === 2 ? (
+                      <span className="testimonial__perfil-stars">★★</span>
+                    ) : commet.rating === 3 ? (
+                      <span className="testimonial__perfil-stars">★★★</span>
+                    ) : commet.rating === 4 ? (
+                      <span className="testimonial__perfil-stars">★★★★</span>
+                    ) : commet.rating === 5 ? (
+                      <span className="testimonial__perfil-stars">★★★★★</span>
+                    ) : null}
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </div>
+          <div className="d-flex gap-3">
+            <div className="prev">
+              <TbPlayerTrackPrev />
             </div>
-            <div className="d-flex gap-3">
-            <div class="prev">
-                <TbPlayerTrackPrev />
+            <div className="next">
+              <TbPlayerTrackNext />
             </div>
-            <div class="next">
-                <TbPlayerTrackNext />
-            </div>
-            </div>
+          </div>
         </Swiper>
-                
       </div>
       <div
         className="col-12 col-md-6 my-5 container__img_header"
@@ -95,7 +95,6 @@ export default function Review() {
       >
         <img src={commentSVG} alt="header" />
       </div>
-      
     </div>
   );
 }
